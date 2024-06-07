2cdfb01bc23c9c29170134c74e688312587421f5
Kevin Ritthaworn <rj.ritthaworn@icloud.com>
2024-06-07 14:54:06 -0400

    renders json and score

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,16 @@
+import { useState } from "react";
+
 function Cards(props) {
   return (
     <>
       <div>
           {props.pokemons.map((pokemon) => {
-            return <button key={pokemon}>{pokemon}</button>;
+            return <button key={pokemon.name} onClick={() => props.onClick(pokemon)}>{pokemon.name}</button>;
           })}
       </div>
     </>
   );
 }
-
-
 
   const shuffle = (array) => {
     for (var i = array.length - 1; i > 0; i--) {
@@ -20,23 +20,36 @@ function Cards(props) {
       return array;
   }
 
-
-
-
 export default function app() {
-  const pokemons = ["pikachu", "charmander", "bulbasour"]
-  shuffle(pokemons)
+  const [pokemons, setPokemon] = useState([
+    {name: "pikachu"},
+    {name: "bulbasour"},
+    {name: "charmander"}
+   ])
+
+  const [score, setScore] = useState({score: 0, bestscore: 0})
+
+  const [isclicked, setIsClicked] = useState(false)
+
+  const handleClick = () => {
+    const shuffledPokemons = shuffle([...pokemons])
+    setPokemon(shuffledPokemons)
+    setIsClicked(true)
+    setScore((prevScore) => ({ ...prevScore, score: prevScore.score + 1}))
+    if (isclicked === true)
+      console.log("you lose")
+  }
 
   return (
   <>
     <div>
       <h1 style= {{fontSize: 25}}>Pokemon Memory Game</h1>
       <p style = {{fontSize: 10}}>Get points by clicking on an image but don't click on any more than once!</p>
-      <Cards pokemons={pokemons}/>
-    </div>
-    <div>
-
+      <p>{score.score}</p>
+      <p>{score.bestscore}</p>
+      <Cards pokemons={pokemons} onClick={handleClick}/>
     </div>
   </>
   )
 }
+
