50fe29767f998df84e46ef9069066236b8bc15d4
Kevin Ritthaworn <rj.ritthaworn@icloud.com>
2024-06-11 17:03:48 -0400

    added pictures, more pokemon and fixed scoring

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ function Cards(props) {
     <>
       <div>
           {props.pokemons.map((pokemon) => {
-            return <button key={pokemon.name} onClick={() => props.onClick(pokemon)}>{pokemon.name}</button>;
+            return <button key={pokemon.name} onClick={() => props.onClick(pokemon)}>{pokemon.name}<img src = {pokemon.url} width={50} height={50}/></button>;
           })}
       </div>
     </>
@@ -22,22 +22,36 @@ function Cards(props) {
 
 export default function app() {
   const [pokemons, setPokemon] = useState([
-    {name: "pikachu"},
-    {name: "bulbasour"},
-    {name: "charmander"}
+    {name: "pikachu", click: false, url: "https://www.pokemon.com/static-assets/content-assets/cms2/img/pokedex/site_search/025.png"},
+    {name: "bulbasaur", click: false, url: "https://www.pokemon.com/static-assets/content-assets/cms2/img/pokedex/site_search/001.png"},
+    {name: "charmander", click: false, url: "https://www.pokemon.com/static-assets/content-assets/cms2/img/pokedex/site_search/004.png"},
+    {name: "lucario", click: false, url: "https://www.pokemon.com/static-assets/content-assets/cms2/img/pokedex/site_search/448.png"},
+    {name: "squirtle", click: false, url: "https://www.pokemon.com/static-assets/content-assets/cms2/img/pokedex/site_search/007.png"},
+    {name: "snivy", click: false, url: "https://www.pokemon.com/static-assets/content-assets/cms2/img/pokedex/site_search/495.png"},
+    {name: "rayquaza", click: false, url: "https://www.pokemon.com/static-assets/content-assets/cms2/img/pokedex/site_search/384.png"},
+    {name: "mewtwo", click: false, url: "https://www.pokemon.com/static-assets/content-assets/cms2/img/pokedex/site_search/150.png"},
+    {name: "zekrom", click: false, url: "https://www.pokemon.com/static-assets/content-assets/cms2/img/pokedex/site_search/644.png"},
+    {name: "groudon", click: false, url: "https://www.pokemon.com/static-assets/content-assets/cms2/img/pokedex/site_search/383.png"},
    ])
 
   const [score, setScore] = useState({score: 0, bestscore: 0})
 
-  const [isclicked, setIsClicked] = useState(false)
-
-  const handleClick = () => {
-    const shuffledPokemons = shuffle([...pokemons])
-    setPokemon(shuffledPokemons)
-    setIsClicked(true)
-    setScore((prevScore) => ({ ...prevScore, score: prevScore.score + 1}))
-    if (isclicked === true)
-      console.log("you lose")
+  const handleClick = (clickedPokemon) => {
+    if (clickedPokemon.click === true) {
+      if (score.score > score.bestscore) {
+        setScore((prevBestScore) => ({ ...prevBestScore, score: prevBestScore.bestscore = score.score})) 
+        setScore((prevScore) => ({ ...prevScore, score: prevScore.score = 0})) 
+      }
+      const poketrue = pokemons.map((pokemon) => { return {...pokemon, click: false }});
+      setPokemon([...poketrue])
+      setScore((prevScore) => ({ ...prevScore, score: prevScore.score = 0})) 
+  
+    } else {
+      const poketrue = pokemons.map((pokemon) => pokemon.name === clickedPokemon.name ? { ...pokemon, click: true } : pokemon );
+      const shuffledPokemons = shuffle([...poketrue])
+      setPokemon(shuffledPokemons)
+      setScore((prevScore) => ({ ...prevScore, score: prevScore.score + 1}))
+    }
   }
 
   return (
@@ -45,11 +59,10 @@ export default function app() {
     <div>
       <h1 style= {{fontSize: 25}}>Pokemon Memory Game</h1>
       <p style = {{fontSize: 10}}>Get points by clicking on an image but don't click on any more than once!</p>
-      <p>{score.score}</p>
-      <p>{score.bestscore}</p>
+      <p>Score: {score.score}</p>
+      <p>Best score: {score.bestscore}</p>
       <Cards pokemons={pokemons} onClick={handleClick}/>
     </div>
   </>
   )
 }
-
